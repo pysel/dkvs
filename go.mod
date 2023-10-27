ea44658f3bc21291db0640d10919d0288315963f
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-10-26 19:39:27 -0700

    gen proto + fix script

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/pysel/dkvs
 go 1.20
 
 require (
-	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/syndtr/goleveldb v1.0.0
+	google.golang.org/protobuf v1.31.0
 )
+
+require github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
--- scripts/protogen.py
+++ scripts/protogen.py
@@ -6,25 +6,46 @@ def get_go_module_name(filepath='go.mod'):
         for line in lines:
             if line.startswith('module'):
                 return line.split()[1].strip()
-            
+
+def list_files_recursively(start_directory):
+    for root, _, files in os.walk(start_directory):
+        for filename in files:
+            # full path of the file
+            full_path = os.path.join(root, filename)
+            yield full_path
+
 proto_dir = "proto/dkvs"
 go_mod_name = get_go_module_name()
-
-for filename in os.listdir(proto_dir):
+subPaths = []
+for filename in list_files_recursively(proto_dir):
     if filename.endswith(".proto"):
-        with open(os.path.join(proto_dir, filename)) as f:
+        with open(filename) as f:
             lines = f.readlines()
             for line in lines:
                 if line.startswith('option go_package'):
                     # Extract the go_package value
                     go_package = line.split('=')[1].strip(' ";').replace('";\n', '')
-                    print(f'go_package: {go_package}')
                     
                     if not os.path.exists(go_package):
                         os.system(f'mkdir -p {go_package}')
                         
                     # Build the protoc command
-                    cmd = f'protoc -I=proto --go_out=. {proto_dir}/{filename}'
+                    cmd = f'protoc -I=proto --go_out=. {filename}'
+                    subPaths.append(filename.split(proto_dir)[1])
                     print(f'Running: {cmd}')
                     os.system(cmd)
                     break
+
+# Remove module prefix and move generated files from there
+for path in subPaths:
+    # get names of top directories to move
+    topDir = path.split('/')[1]
+    dirToMove = go_mod_name + "/" + topDir
+    cmd = f'(mkdir {topDir}; mv {dirToMove} .)'
+    print(f'Running: {cmd}')
+    os.system(cmd)
+
+topModDir = go_mod_name.split("/")[0]
+rmrfcmd = f"rm -rf {topModDir}"
+print(f'Running: {rmrfcmd}')
+os.system(rmrfcmd)
