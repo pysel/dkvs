8ff21f7fadee18ea81d806a12d3033d012ec15b5
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-10-26 20:18:22 -0700

    partition db methods

--- partition/keys.go
+++ partition/keys.go
@@ -3,7 +3,7 @@ package partition
 import "bytes"
 
 func (p *Partition) checkKeyRange(key []byte) error {
-	if bytes.Compare(key, p.hashRange.Min.Bytes()) == -1 || bytes.Compare(key, p.hashRange.Max.Bytes()) == 1 {
+	if bytes.Compare(key, p.hashrange.Min.Bytes()) == -1 || bytes.Compare(key, p.hashrange.Max.Bytes()) == 1 {
 		// Key is out of range.
 		return ErrNotThisPartitionKey
 	}
--- partition/partition.go
+++ partition/partition.go
@@ -6,8 +6,9 @@ import (
 
 // Partition is a slave node that stores some range of keys
 type Partition struct {
-	DB        db.DB
-	hashRange *Range
+	hashrange *Range
+
+	db.DB
 }
 
 // NewPartition creates a new partition instance.
@@ -18,8 +19,8 @@ func NewPartition(dbPath string, hashRange *Range) *Partition {
 	}
 
 	return &Partition{
-		db,
 		hashRange,
+		db,
 	}
 }
 
@@ -27,9 +28,34 @@ func NewPartition(dbPath string, hashRange *Range) *Partition {
 // Keys should be sent of 32 length bytes, since SHA-2 produces 256-bit hashes, and be of big endian format.
 
 func (p *Partition) Get(key []byte) ([]byte, error) {
-	if err := checkKeyRange(key); err != nil {
+	if err := p.checkKeyRange(key); err != nil {
 		return nil, ErrNotThisPartitionKey
 	}
 
 	return p.DB.Get(key)
+}
+
+func (p *Partition) Set(key, value []byte) error {
+	if err := p.checkKeyRange(key); err != nil {
+		return ErrNotThisPartitionKey
+	}
+
+	return p.DB.Set(key, value)
+}
+
+func (p *Partition) Delete(key []byte) error {
+	if err := p.checkKeyRange(key); err != nil {
+		return ErrNotThisPartitionKey
+	}
+
+	return p.DB.Delete(key)
+}
+
+func (p *Partition) Has(key []byte) (bool, error) {
+	if err := p.checkKeyRange(key); err != nil {
+		// still error to be able to distinguish between not found and out of range
+		return false, ErrNotThisPartitionKey
+	}
+
+	return p.DB.Has(key), nil
 }
