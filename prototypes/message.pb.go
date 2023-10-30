bdba9525c3fbfa5eab8b618774da883e258331ed
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-10-29 20:02:03 -0700

    fix generated proto

--- prototypes/message.pb.go
+++ prototypes/message.pb.go
@@ -21,7 +21,7 @@ const (
 )
 
 // StoreMessage is a message that is sent to a node that should store a key
-type StoreMessage struct {
+type StoreMessageRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
@@ -30,8 +30,8 @@ type StoreMessage struct {
 	Value string `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
 }
 
-func (x *StoreMessage) Reset() {
-	*x = StoreMessage{}
+func (x *StoreMessageRequest) Reset() {
+	*x = StoreMessageRequest{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_dkvs_message_message_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -39,13 +39,13 @@ func (x *StoreMessage) Reset() {
 	}
 }
 
-func (x *StoreMessage) String() string {
+func (x *StoreMessageRequest) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*StoreMessage) ProtoMessage() {}
+func (*StoreMessageRequest) ProtoMessage() {}
 
-func (x *StoreMessage) ProtoReflect() protoreflect.Message {
+func (x *StoreMessageRequest) ProtoReflect() protoreflect.Message {
 	mi := &file_dkvs_message_message_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -57,23 +57,61 @@ func (x *StoreMessage) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use StoreMessage.ProtoReflect.Descriptor instead.
-func (*StoreMessage) Descriptor() ([]byte, []int) {
+// Deprecated: Use StoreMessageRequest.ProtoReflect.Descriptor instead.
+func (*StoreMessageRequest) Descriptor() ([]byte, []int) {
 	return file_dkvs_message_message_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *StoreMessage) GetKey() string {
+func (x *StoreMessageRequest) GetKey() string {
 	if x != nil {
 		return x.Key
 	}
 	return ""
 }
 
-func (x *StoreMessage) GetValue() string {
+func (x *StoreMessageRequest) GetValue() string {
 	if x != nil {
 		return x.Value
 	}
 	return ""
+}
+
+type StoreMessageResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *StoreMessageResponse) Reset() {
+	*x = StoreMessageResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_dkvs_message_message_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StoreMessageResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StoreMessageResponse) ProtoMessage() {}
+
+func (x *StoreMessageResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_dkvs_message_message_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StoreMessageResponse.ProtoReflect.Descriptor instead.
+func (*StoreMessageResponse) Descriptor() ([]byte, []int) {
+	return file_dkvs_message_message_proto_rawDescGZIP(), []int{1}
 }
 
 var File_dkvs_message_message_proto protoreflect.FileDescriptor
@@ -81,13 +119,15 @@ var File_dkvs_message_message_proto protoreflect.FileDescriptor
 var file_dkvs_message_message_proto_rawDesc = []byte{
 	0x0a, 0x1a, 0x64, 0x6b, 0x76, 0x73, 0x2f, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2f, 0x6d,
 	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0c, 0x64, 0x6b,
-	0x76, 0x73, 0x2e, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x36, 0x0a, 0x0c, 0x53, 0x74,
-	0x6f, 0x72, 0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65,
-	0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05,
-	0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c,
-	0x75, 0x65, 0x42, 0x22, 0x5a, 0x20, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
-	0x2f, 0x70, 0x79, 0x73, 0x65, 0x6c, 0x2f, 0x64, 0x6b, 0x76, 0x73, 0x2f, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x76, 0x73, 0x2e, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x3d, 0x0a, 0x13, 0x53, 0x74,
+	0x6f, 0x72, 0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03,
+	0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x16, 0x0a, 0x14, 0x53, 0x74, 0x6f,
+	0x72, 0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
+	0x65, 0x42, 0x22, 0x5a, 0x20, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f,
+	0x70, 0x79, 0x73, 0x65, 0x6c, 0x2f, 0x64, 0x6b, 0x76, 0x73, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x74, 0x79, 0x70, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -102,9 +142,10 @@ func file_dkvs_message_message_proto_rawDescGZIP() []byte {
 	return file_dkvs_message_message_proto_rawDescData
 }
 
-var file_dkvs_message_message_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_dkvs_message_message_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_dkvs_message_message_proto_goTypes = []interface{}{
-	(*StoreMessage)(nil), // 0: dkvs.message.StoreMessage
+	(*StoreMessageRequest)(nil),  // 0: dkvs.message.StoreMessageRequest
+	(*StoreMessageResponse)(nil), // 1: dkvs.message.StoreMessageResponse
 }
 var file_dkvs_message_message_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -121,7 +162,19 @@ func file_dkvs_message_message_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_dkvs_message_message_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*StoreMessage); i {
+			switch v := v.(*StoreMessageRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_dkvs_message_message_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StoreMessageResponse); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -139,7 +192,7 @@ func file_dkvs_message_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_dkvs_message_message_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
