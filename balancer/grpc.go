8f358658bc9324be666a78ad2900297147d00587
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-11-10 17:44:10 -0800

    register partition grpc

--- balancer/grpc.go
+++ balancer/grpc.go
@@ -7,5 +7,16 @@ import (
 )
 
 func (b *BalancerServer) RegisterPartition(ctx context.Context, req *pbbalancer.RegisterPartitionRequest) (*pbbalancer.RegisterPartitionResponse, error) {
-	return nil, nil
+	range_, err := b.getNextPartitionRange()
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.Balancer.RegisterPartition(req.Address, *range_)
+	if err != nil {
+		return nil, err
+	}
+
+	// partition successfully registered
+	return &pbbalancer.RegisterPartitionResponse{}, nil
 }
