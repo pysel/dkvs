48d1686d877b3aef378f98c85eb20b3222459d88
Ruslan Akhtariev <rakhtariev@icloud.com>
2024-02-03 13:49:45 -0800

    types: refactor backlog

--- types/backlog.go
+++ types/backlog.go
@@ -4,47 +4,43 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type messages ([]struct {
+// Backlog is a list of messages and corresponding timestamps.
+type Backlog ([]struct {
 	timestamp uint64
 	msg       proto.Message
 })
-
-// Add appends a message to messages and preserves the ascending order of timestamps
-func (m *messages) Add(ts uint64, message proto.Message) {
-	for i, msg := range *m {
-		if msg.timestamp > ts {
-			*m = append((*m)[:i], append([]struct {
-				timestamp uint64
-				msg       proto.Message
-			}{{ts, message}}, (*m)[i:]...)...)
-			return
-		}
-	}
-}
-
-// Backlog is a mapping of client id to messages.
-// Messages are stored in ascending order of timestamps.
-type Backlog (map[string]messages)
 
 func NewBacklog() *Backlog {
 	return new(Backlog)
 }
 
-// Add appends a message to ID's backlog and preserves the ascending order of timestamps.
-func (b *Backlog) Add(id string, ts uint64, message proto.Message) {
-	messages := (*b)[id]
-	messages.Add(ts, message)
-}
-
-// Pop removes and returns the first message with timestamp less than or equal to ts.
-func (b *Backlog) Pop(id string, ts uint64) proto.Message {
-	messages := (*b)[id]
-	for i, msg := range messages {
-		if msg.timestamp <= ts {
-			(*b)[id] = append(messages[:i], messages[i+1:]...)
-			return msg.msg
+// Add appends a message to messages and preserves the ascending order of timestamps
+func (b *Backlog) Add(ts uint64, message proto.Message) {
+	element := struct {
+		timestamp uint64
+		msg       proto.Message
+	}{ts, message}
+	for i, msg := range *b {
+		if msg.timestamp > ts {
+			*b = append(
+				append((*b)[:i], element), // append element to the slice of a messages with lower timestamps
+				(*b)[i:]...,               // append the rest of the messages
+			)
+			return
 		}
 	}
 
-	return nil
+	// if the message has the highest timestamp, append it to the end
+	*b = append(*b, element)
+}
+
+// Pop returns the first message (the one with the smallest timestamp) and removes it from the backlog.
+func (b *Backlog) Pop() proto.Message {
+	if len(*b) == 0 {
+		return nil
+	}
+
+	msg := (*b)[0].msg
+	*b = (*b)[1:]
+	return msg
 }
